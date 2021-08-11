7c5bf1d50f09619f7c0d67f8e2d61ae66cbb1897
Emil Darakchiev <72097603+edarakchiev@users.noreply.github.com>
2021-08-11 23:21:54 +0300

    Add workout tests

--- tests/workouts/test_view.py
+++ tests/workouts/test_view.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
 
+from fitness.fitness_workout_app.models import Workout
 from tests.base.mixins import WorkoutTestUtils
 from tests.base.tests import FitnessTestCase
 
@@ -37,6 +38,13 @@ class WorkoutDetailsTest(WorkoutTestUtils, FitnessTestCase):
         response = self.client.get(reverse('workout details', kwargs={'pk': workout.id}))
 
         self.assertFalse(response.context['is_owner'])
+
+    def test_details_statusCode(self):
+        self.client.force_login(self.user)
+        response = self.client.get('')
+
+        self.assertEqual(response.status_code, 200)
+
     def test_indexView_when_userIsLoggedIn(self):
         self.client.force_login(self.user)
 
@@ -54,4 +62,70 @@ class WorkoutDetailsTest(WorkoutTestUtils, FitnessTestCase):
 
         self.assertEqual(list(response.context['workouts']), [workout])
 
+    def test_IndexTemplateUsed(self):
+        response = self.client.get(reverse('index'))
+        self.assertTemplateUsed(response, 'index.html')
 
+    def test_numberOfWorkouts(self):
+        self.client.force_login(self.user)
+        self.assertEqual(Workout.objects.all().count(), 0)
+
+    def test_data(self):
+        workout = Workout.objects.create(muscle_group='Chest', title='test title', schema='image.png', series=3,
+                                         repetitions=7, description='test description', user=self.user)
+
+        self.assertEqual(workout.muscle_group, 'Chest')
+        self.assertEqual(workout.title, 'test title')
+        self.assertEqual(workout.schema, 'image.png')
+        self.assertEqual(workout.series, 3)
+        self.assertEqual(workout.repetitions, 7)
+        self.assertEqual(workout.description, 'test description')
+
+    def test_statusCodeEdit_whenLoggedInUserAndUserIsOwner(self):
+        workout = Workout.objects.create(muscle_group='Chest', title='test title', schema='image.png', series=3,
+                                         repetitions=7, description='test description', user=self.user)
+
+        self.client.force_login(self.user)
+
+        response = self.client.get(reverse('edit workout', kwargs={'pk': workout.id}))
+
+        self.assertEqual(workout.user, self.user)
+        self.assertEqual(response.status_code, 200)
+
+    def test_deleteWorkout(self):
+        self.client.force_login(self.user)
+
+        workout = Workout.objects.create(muscle_group='Chest', title='test title', schema='image.png', series=3,
+                                         repetitions=7, description='test description', user=self.user)
+
+        get_response = self.client.get(reverse('delete workout', kwargs={'pk': workout.id}), follow=True)
+        self.assertContains(get_response, 'Delete Workouts')
+
+        post_response = self.client.post(reverse('delete workout', kwargs={'pk': workout.id}), follow=True)
+        self.assertRedirects(post_response, reverse('index'), status_code=302)
+
+    def test_createView(self):
+        self.client.force_login(self.user)
+
+        response = self.client.post(reverse('create workout'),
+                                        {
+            'muscle_group': 'Chest',
+            'title': 'test title',
+            'schema': 'image.png',
+            'series': 3,
+            'repetitions': 7,
+            'description': 'test description',
+            'user': self.user
+        }
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_CreateTemplateUsed(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('create workout'))
+        self.assertTemplateUsed(response, 'create_workout.html')
+
+    def test_statusCodeCreate_whenLoggedInUser(self):
+        self.client.force_login(self.user)
+        response = self.client.post('/create_workout/')
+        self.assertEqual(response.status_code, 200)
